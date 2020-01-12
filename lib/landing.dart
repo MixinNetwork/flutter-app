66c36fc7b5ebb5df2c0a657cd552234a9546359d
Crossle Song <crosslesong@gmail.com>
2020-01-12 23:24:42 +0800

    Landing url generate

--- lib/landing.dart
+++ lib/landing.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:cryptography/cryptography.dart';
+import 'package:uuid/uuid.dart';
 
 class LandingPage extends StatefulWidget {
   LandingPage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -22,12 +15,10 @@ class LandingPage extends StatefulWidget {
 class _MyHomePageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var uuid = Uuid().v4();
+    final keypair = x25519.newKeyPair();
+    var pubKey = Uri.encodeComponent(keypair.publicKey.toBase64());
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -62,7 +53,7 @@ class _MyHomePageState extends State<LandingPage> {
               height: 30,
             ),
             QrImage(
-              data: "mixin://auth/device/",
+              data: "mixin://device/auth?id="+uuid + "&pub_key=" + pubKey,
               version: QrVersions.auto,
               size: 300.0,
               foregroundColor: Color(0xFF4A4A4A),
