6b58a7438b2b84da9b8cc591c8c0c34be3499661
Crossle Song <crosslesong@gmail.com>
2020-07-07 00:14:25 +0800

    Update library

--- lib/app.dart
+++ lib/app.dart
@@ -7,6 +7,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
