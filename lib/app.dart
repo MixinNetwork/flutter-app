de28e3ff9e47876e6315c9c7bcdaa9e69a3d817c
Crossle Song <crosslesong@gmail.com>
2020-07-30 21:47:45 +0800

    Update flutter

--- lib/app.dart
+++ lib/app.dart
@@ -2,22 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/landing.dart';
 
 class App extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: LandingPage(title: 'Mixin Messenger'),
