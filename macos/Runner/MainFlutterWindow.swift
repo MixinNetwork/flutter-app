13163c8927d203e8b295f2e01584e3e285013004
SeniorZhai <developer.zhaitao@gmail.com>
2020-12-04 23:51:26 +0800

    Update window style

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -5,10 +5,13 @@ class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
+    self.minSize = NSSize(width: 560, height: 320)
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
-
     RegisterGeneratedPlugins(registry: flutterViewController)
+    self.titleVisibility = .hidden
+    self.titlebarAppearsTransparent = true
+    self.styleMask = [self.styleMask, NSWindow.StyleMask.fullSizeContentView]
 
     super.awakeFromNib()
   }
