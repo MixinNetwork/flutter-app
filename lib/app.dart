be9cd9e0237e7ce7e043232d891b7369ddd3c29e
YeungKC <I@YeungKC.com>
2021-01-05 01:43:47 +0800

    Fix sdk reference.

--- lib/app.dart
+++ lib/app.dart
@@ -9,7 +9,6 @@ import 'package:flutter_app/ui/landing/landing.dart';
 import 'package:flutter_app/utils/Preferences.dart';
 import 'package:flutter_app/widgets/brightness_observer.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hive/hive.dart';
 
 import 'mixin_client.dart';
 
