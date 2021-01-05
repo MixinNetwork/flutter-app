f72c47f7fa4357189038b9ad5a51bb8354b6e268
YeungKC <I@YeungKC.com>
2021-01-06 01:50:21 +0800

    Improve landing_cubit.

--- lib/ui/landing/bloc/landing_cubit.dart
+++ lib/ui/landing/bloc/landing_cubit.dart
@@ -10,13 +10,14 @@ import 'package:flutter_app/ui/home/bloc/auth_cubit.dart';
 import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart' as signal;
 import 'package:ed25519_edwards/ed25519_edwards.dart' as ed;
 import 'package:mixin_bot_sdk_dart/mixin_bot_sdk_dart.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:tuple/tuple.dart';
 
 part 'landing_state.dart';
 
 class LandingCubit extends Cubit<LandingState> with SubscribeMixin {
   LandingCubit(this.authCubit) : super(const LandingState()) {
-    _initVerify();
+    _initLandingListen();
     requestAuthUrl();
   }
 
@@ -56,81 +57,89 @@ class LandingCubit extends Cubit<LandingState> with SubscribeMixin {
     }
   }
 
-  void _initVerify() {
-    final secretStream =
-        periodicStreamController.stream.asyncMap((event) async {
-      final rsp =
-          await MixinClient().client.provisioningApi.getProvisioning(deviceId);
-      return rsp?.data?.secret;
-    }).where((event) => event?.isNotEmpty == true);
-
-    final authStream = secretStream.asyncMap((secret) async {
-      final result =
-          signal.decrypt(base64.encode(keyPair.privateKey.serialize()), secret);
-      final msg = json.decode(String.fromCharCodes(result));
-
-      final edKeyPair = ed.generateKey();
-      final registrationId = signal.KeyHelper.generateRegistrationId(false);
-
-      final rsp = await MixinClient().client.provisioningApi.verifyProvisioning(
-            ProvisioningRequest(
-              code: msg['provisioning_code'],
-              userId: msg['user_id'],
-              sessionId: msg['session_id'],
-              platform: 'Desktop',
-              purpose: 'SESSION',
-              sessionSecret: base64.encode(edKeyPair.publicKey.bytes),
-              appVersion: '0.0.1',
-              registrationId: registrationId,
-              platformVersion: 'OS X 10.15.6',
+  void _initLandingListen() {
+    final subscription = periodicStreamController.stream
+        .doOnData((event) {
+          if (event < 60) return;
+          streamSubscription?.cancel();
+          emit(state.copyWith(
+            status: LandingStatus.needRetry,
+          ));
+        })
+        .asyncMap((event) async {
+          final rsp = await MixinClient()
+              .client
+              .provisioningApi
+              .getProvisioning(deviceId);
+          return rsp?.data?.secret;
+        })
+        .handleError((_) => null)
+        .where((secret) => secret?.isNotEmpty == true)
+        .doOnData((secret) {
+          streamSubscription?.cancel();
+          emit(state.copyWith(
+            status: LandingStatus.provisioning,
+          ));
+        })
+        .asyncMap(_verify)
+        .handleError((_) => null)
+        .doOnData((auth) {
+          if (auth == null) {
+            streamSubscription?.cancel();
+            emit(state.copyWith(
+              status: LandingStatus.needRetry,
+            ));
+          }
+        })
+        .where((auth) => auth != null)
+        .listen((auth) {
+          MixinClient().client.initMixin(
+                auth.item1.userId,
+                auth.item1.sessionId,
+                auth.item2,
+                'PROFILE:READ PROFILE:WRITE PHONE:READ PHONE:WRITE CONTACTS:READ CONTACTS:WRITE MESSAGES:READ MESSAGES:WRITE ASSETS:READ SNAPSHOTS:READ CIRCLES:READ CIRCLES:WRITE',
+              );
+          authCubit.emit(
+            authCubit.state.copyWith(
+              account: auth.item1,
+              privateKey: auth.item2,
             ),
           );
+        });
+    addSubscription(subscription);
+  }
 
-      if (rsp.data != null) {
-        final privateKey = base64.encode(edKeyPair.privateKey.bytes);
+  FutureOr<Tuple2<Account, String>> _verify(secret) async {
+    final result =
+        signal.decrypt(base64.encode(keyPair.privateKey.serialize()), secret);
+    final msg = json.decode(String.fromCharCodes(result));
 
-        return Tuple2(
-          rsp.data,
-          privateKey,
+    final edKeyPair = ed.generateKey();
+    final registrationId = signal.KeyHelper.generateRegistrationId(false);
+
+    final rsp = await MixinClient().client.provisioningApi.verifyProvisioning(
+          ProvisioningRequest(
+            code: msg['provisioning_code'],
+            userId: msg['user_id'],
+            sessionId: msg['session_id'],
+            platform: 'Desktop',
+            purpose: 'SESSION',
+            sessionSecret: base64.encode(edKeyPair.publicKey.bytes),
+            appVersion: '0.0.1',
+            registrationId: registrationId,
+            platformVersion: 'OS X 10.15.6',
+          ),
         );
-      }
-      return null;
-    });
 
-    addSubscription(periodicStreamController.stream
-        .where((event) => event >= 60)
-        .listen((event) {
-      streamSubscription?.cancel();
-      emit(state.copyWith(
-        status: LandingStatus.needRetry,
-      ));
-    }));
+    if (rsp.data != null) {
+      final privateKey = base64.encode(edKeyPair.privateKey.bytes);
 
-    addSubscription(secretStream.listen((event) {
-      streamSubscription?.cancel();
-      emit(state.copyWith(
-        status: LandingStatus.provisioning,
-      ));
-    }));
-
-    addSubscription(authStream
-        .where((event) => event == null)
-        .listen((event) => emit(state.copyWith(
-              status: LandingStatus.needRetry,
-            ))));
-
-    addSubscription(authStream.where((event) => event != null).listen((event) {
-      MixinClient().client.initMixin(
-            event.item1.userId,
-            event.item1.sessionId,
-            event.item2,
-            'PROFILE:READ PROFILE:WRITE PHONE:READ PHONE:WRITE CONTACTS:READ CONTACTS:WRITE MESSAGES:READ MESSAGES:WRITE ASSETS:READ SNAPSHOTS:READ CIRCLES:READ CIRCLES:WRITE',
-          );
-      authCubit.emit(authCubit.state.copyWith(
-        account: event.item1,
-        privateKey: event.item2,
-      ));
-    }));
+      return Tuple2(
+        rsp.data,
+        privateKey,
+      );
+    }
+    return null;
   }
 
   @override
