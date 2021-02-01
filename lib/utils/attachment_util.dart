db1fcccd86a0b109605577bccc1975fee14733f0
SeniorZhai <developer.zhaitao@gmail.com>
2021-02-01 22:28:52 +0800

    Upload attachment

--- lib/utils/attachment_util.dart
+++ lib/utils/attachment_util.dart
@@ -1,15 +1,12 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_app/db/mixin_database.dart';
 import 'package:mixin_bot_sdk_dart/mixin_bot_sdk_dart.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_app/db/extension/message_category.dart';
-
-// ignore: prefer_generic_function_type_aliases
-typedef void OnDownloadProgressCallback(int receivedBytes, int totalBytes);
-// ignore: prefer_generic_function_type_aliases
-typedef void OnUploadProgressCallback(int sentBytes, int totalBytes);
 
 class AttachmentUtil {
   AttachmentUtil(this._client, this.mediaPath) {
@@ -25,19 +22,84 @@ class AttachmentUtil {
 
   void downloadAttachment(Message message) async {
     final response = await _client.attachmentApi.getAttachment(message.content);
-    if (response.data != null) {
-      await _attachmentClient
-          .getUrl(Uri.parse(response.data.viewUrl))
-          .then((HttpClientRequest request) => request.close())
-          .then((HttpClientResponse response) async {
-        final file = _getAttachmentFile(message);
-        await file.create(recursive: true);
-        await response.pipe(file.openWrite());
-      });
+    if (response.data != null && response.data.viewUrl != null) {
+      final request =
+          await _attachmentClient.getUrl(Uri.parse(response.data.viewUrl));
+
+      request.headers
+          .add(HttpHeaders.contentTypeHeader, 'application/octet-stream');
+
+      final httpResponse = await request.close();
+
+      var byteCount = 0;
+      final totalBytes = httpResponse.contentLength;
+
+      final file = _getAttachmentFile(message);
+      await file.create(recursive: true);
+      final raf = file.openSync(mode: FileMode.write);
+
+      debugPrint('download ${Uri.parse(response.data.viewUrl)}');
+      httpResponse.listen(
+        (data) {
+          byteCount += data.length;
+          raf.writeFromSync(data);
+          // download progress
+          debugPrint('$byteCount / $totalBytes');
+        },
+        onDone: () {
+          raf.closeSync();
+        },
+        onError: (e) {
+          raf.closeSync();
+          file.deleteSync();
+        },
+        cancelOnError: true,
+      );
     }
   }
 
-  void uploadAttachment(Message message) {}
+  void uploadAttachment(File file, String messageId) async {
+    final response = await _client.attachmentApi.postAttachment();
+    if (response.data != null && response.data.uploadUrl != null) {
+      final fileStream = file.openRead();
+
+      final totalBytes = file.lengthSync();
+
+      final request =
+          await _attachmentClient.putUrl(Uri.parse(response.data.uploadUrl));
+
+      request.headers
+        ..add(HttpHeaders.contentTypeHeader, 'application/octet-stream')
+        ..add(HttpHeaders.connectionHeader, 'close')
+        ..add(HttpHeaders.contentLengthHeader, totalBytes)
+        ..add('x-amz-acl', 'public-read');
+
+      var byteCount = 0;
+
+      await request.addStream(fileStream.transform(
+        StreamTransformer.fromHandlers(
+          handleData: (data, sink) {
+            byteCount += data.length;
+            // upload progress
+            debugPrint('$byteCount / $totalBytes');
+            sink.add(data);
+          },
+          handleError: (error, stack, sink) {},
+          handleDone: (sink) {
+            sink.close();
+          },
+        ),
+      ));
+
+      final httpResponse = await request.close();
+      if (httpResponse.statusCode == 200) {
+        // todo
+        debugPrint('upload done!');
+      } else {
+        // todo
+      }
+    }
+  }
 
   File _getAttachmentFile(Message message) {
     assert(message.category.isAttachment);
