529cd0e8cabca5d55f26be87f4279ec72d34f97f
Crossle Song <crosslesong@gmail.com>
2021-02-02 14:00:44 +0800

    Remove unused api package

--- lib/api/base_api.dart
+++
@@ -1,6 +0,0 @@
-import 'package:http/http.dart' as http;
-import 'client.dart';
-
-class BaseAPI {
-  var client = BearerClient('', '', http.Client());
-}
--- lib/api/user_api.dart
+++
@@ -1,5 +0,0 @@
-import 'base_api.dart';
-
-class UserAPI extends BaseAPI {
-   
-}
