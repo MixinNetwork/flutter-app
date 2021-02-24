da1b521268c082f46bf312636fa3e0afb4e4ebf8
YeungKC <I@YeungKC.com>
2021-02-24 16:45:24 +0800

    Text message support highlight, Improve open url.

---
+++ lib/utils/uri_utils.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_app/constants/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'enum_to_string.dart';
+
+Future<bool> openUri(String text) {
+  final uri = Uri.parse(text);
+  if(uri.scheme == null) return Future.value(false);
+
+  if (uri.isScheme(mixinScheme)) {
+    final host = EnumToString.fromString(MixinSchemeHost.values, uri?.host);
+
+    final protocolUrl = mixinProtocolUrls[host];
+    if (protocolUrl != null) {
+      return launch('$protocolUrl${uri.path}');
+    } else if (MixinSchemeHost.users == host) {
+      // TODO user profile page
+      return launch(text);
+    }
+  }
+
+  return launch(text);
+}
