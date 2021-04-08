31354d709e14e174da398d8872a7c22867998e1d
SeniorZhai <developer.zhaitao@gmail.com>
2021-04-08 16:11:53 +0800

    Update uuid test

--- test/uuid_test.dart
+++ test/uuid_test.dart
@@ -12,5 +12,7 @@ void testGenerateConversationId(){
   const a = '80f5130e-a5f5-47e4-80a1-535563687709';
   const b = '508c75a2-ddce-4858-9e66-cf6d58db15cf';
   final result = generateConversationId(a, b);
+  final otherResult = generateConversationId(b, a);
   assert(result == 'a59c170e-02e6-3aa6-8ff1-6cb9350fa8fc');
+  assert(result == otherResult);
 }
