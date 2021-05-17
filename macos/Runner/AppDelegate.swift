913a6287a6eb77f574a55b710eb25d16b26fad26
YeungKC <I@YeungKC.com>
2021-05-17 15:19:24 +0800

    Ignore SIGPIPE.

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -6,4 +6,12 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+
+  override func applicationDidBecomeActive(_ notification: Notification) {
+    signal(SIGPIPE, SIG_IGN);
+  }
+
+  override func applicationWillResignActive(_ notification: Notification) {
+    signal(SIGPIPE, SIG_IGN);
+  }
 }
