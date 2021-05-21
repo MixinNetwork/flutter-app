13c949ef5693a664b9c7e480262c1490666d89f7
YeungKC <I@YeungKC.com>
2021-05-21 15:18:12 +0800

    format code.

--- lib/crypto/privacy_key_value.dart
+++ lib/crypto/privacy_key_value.dart
@@ -1,10 +1,4 @@
-import 'dart:io';
-
-import 'package:flutter_app/utils/hive_key_values.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:path/path.dart' as p;
-import 'package:path_provider/path_provider.dart';
+import '../utils/hive_key_values.dart';
 
 class PrivacyKeyValue extends HiveKeyValue {
   PrivacyKeyValue._() : super(hivePrivacy);
@@ -18,9 +12,11 @@ class PrivacyKeyValue extends HiveKeyValue {
   static const hasPushSignalKeys = 'has_push_signal_keys';
 
   bool getHasSyncSession() => box.get(hasSyncSession, defaultValue: false);
+
   void setHasSyncSession(bool value) => box.put(hasSyncSession, value);
 
   bool getHasPushSignalKeys() =>
       box.get(hasPushSignalKeys, defaultValue: false);
+
   void setHasPushSignalKeys(bool value) => box.put(hasPushSignalKeys, value);
 }
--- lib/ui/setting/about_page.dart
+++ lib/ui/setting/about_page.dart
@@ -2,21 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_app/account/account_server.dart';
-import 'package:flutter_app/crypto/signal/signal_database.dart';
-import 'package:flutter_app/widgets/interacter_decorated_box.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:moor_db_viewer/moor_db_viewer.dart';
 import 'package:package_info/package_info.dart';
 import 'package:provider/provider.dart';
 
+import '../../account/account_server.dart';
 import '../../constants/resources.dart';
+import '../../crypto/signal/signal_database.dart';
 import '../../generated/l10n.dart';
 import '../../utils/hook.dart';
 import '../../utils/uri_utils.dart';
 import '../../widgets/app_bar.dart';
 import '../../widgets/brightness_observer.dart';
 import '../../widgets/cell.dart';
+import '../../widgets/interacter_decorated_box.dart';
 
 class AboutPage extends HookWidget {
   const AboutPage({Key? key}) : super(key: key);
