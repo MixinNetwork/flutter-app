90166631b6b1c1e0e1dca8a99c37159fe217bc9b
YeungKC <I@YeungKC.com>
2021-05-25 14:27:02 +0800

    Fix first received message conversation preview content is null.

--- lib/workers/injector.dart
+++ lib/workers/injector.dart
@@ -24,7 +24,7 @@ class Injector {
       if (unWait) {
         unawaited(_refreshConversation(conversationId));
       } else {
-        _refreshConversation(conversationId);
+        await _refreshConversation(conversationId);
       }
     }
   }
