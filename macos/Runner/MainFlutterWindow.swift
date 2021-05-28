dcaf595f7202d2ccd450ea70fa071b68dcac9409
boyan <yangbinyhbn@gmail.com>
2021-05-28 15:35:15 +0800

    feat/window

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,9 +1,10 @@
+import bitsdojo_window_macos
 import Cocoa
 import FlutterMacOS
 
-class MainFlutterWindow: NSWindow {
+class MainFlutterWindow: BitsdojoWindow {
   override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
+    let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
     self.minSize = NSSize(width: 460, height: 320)
     self.contentViewController = flutterViewController
@@ -12,11 +13,15 @@ class MainFlutterWindow: NSWindow {
     self.titleVisibility = .hidden
     self.titlebarAppearsTransparent = true
     self.styleMask = [self.styleMask, NSWindow.StyleMask.fullSizeContentView]
-    
+
     self.isOpaque = false
-    self.backgroundColor = NSColor.init(calibratedWhite: 1.0, alpha: 0.5)
+    self.backgroundColor = NSColor(calibratedWhite: 1.0, alpha: 0.5)
     self.isReleasedWhenClosed = false
-    
+
     super.awakeFromNib()
+  }
+
+  override func bitsdojo_window_configure() -> UInt {
+    return BDW_CUSTOM_FRAME | BDW_HIDE_ON_STARTUP
   }
 }
