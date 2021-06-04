3f6eca71847f7c69b81ffe3207f4b0b448a53203
SeniorZhai <developer.zhaitao@gmail.com>
2021-06-04 20:19:37 +0800

    Sync read status

--- lib/workers/decrypt_message.dart
+++ lib/workers/decrypt_message.dart
@@ -837,6 +837,10 @@ class DecryptMessage extends Injector {
     }
     await database.jobsDao.insertNoReplace(
         createAckJob(acknowledgeMessageReceipts, messageId, status));
+    if (status == MessageStatus.read) {
+      await database.jobsDao
+          .insert(createAckJob(createMessage, messageId, MessageStatus.read));
+    }
   }
 
   Future<void> _markMessageStatus(List<BlazeAckMessage> messages) async {
