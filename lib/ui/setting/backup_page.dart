e5cfc71bc11e5d5ed6e9b51779c84fedc1ac7a89
boyan <yangbinyhbn@gmail.com>
2021-06-10 17:35:03 +0800

    fix analyze

--- lib/ui/setting/backup_page.dart
+++ lib/ui/setting/backup_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_app/ui/setting/widgets/theme.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -10,6 +9,7 @@ import '../../generated/l10n.dart';
 import '../../widgets/app_bar.dart';
 import '../../widgets/brightness_observer.dart';
 import '../../widgets/cell.dart';
+import 'widgets/theme.dart';
 
 class BackupPage extends HookWidget {
   const BackupPage({Key? key}) : super(key: key);
