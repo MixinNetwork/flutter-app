09719a537e97d9ef5512aec0768e1453a8e5c2de
YeungKC <I@YeungKC.com>
2021-06-10 19:05:24 +0800

    Fix (#67)

--- lib/widgets/input_container.dart
+++ lib/widgets/input_container.dart
@@ -306,6 +306,7 @@ class _SendTextField extends StatelessWidget {
                 child: TextField(
                   maxLines: 7,
                   minLines: 1,
+                  autofocus: true,
                   controller: context.watch<TextEditingController>(),
                   style: TextStyle(
                     color: BrightnessData.themeOf(context).text,
