93f0549996b6188beab134ceaf2dd15092739c7c
YeungKC <I@YeungKC.com>
2021-06-12 11:32:51 +0800

    format code.

--- lib/widgets/input_container.dart
+++ lib/widgets/input_container.dart
@@ -291,27 +291,18 @@ class _SendTextField extends HookWidget {
     final sendable = useStream(
           useMemoized(
               () => Rx.combineLatest2<TextEditingValue, MentionState, bool>(
-                      textEditingValueStream
-                          .startWith(textEditingController.value),
-                      mentionStream.startWith(mentionCubit.state),
-                      (textEditingValue, mentionState) {
-                    final foo = (textEditingValue.text.trim().isNotEmpty) &&
-                        (textEditingValue.composing.composed);
-                    final bar = mentionState.text == null;
-                    final sendable = foo && bar;
-                    print('fuck foo: $foo');
-                    print('fuck bar: $bar');
-                    print('fuck _sendable: $sendable');
-                    return sendable;
-                  }).distinct(),
+                  textEditingValueStream.startWith(textEditingController.value),
+                  mentionStream.startWith(mentionCubit.state),
+                  (textEditingValue, mentionState) =>
+                      (textEditingValue.text.trim().isNotEmpty) &&
+                      (textEditingValue.composing.composed) &&
+                      mentionState.text == null).distinct(),
               [
                 textEditingValueStream,
                 mentionStream,
               ]),
         ).data ??
         true;
-
-    print('fuck sendable: $sendable');
 
     return Container(
       constraints: const BoxConstraints(minHeight: 40),
@@ -325,7 +316,6 @@ class _SendTextField extends HookWidget {
       ),
       alignment: Alignment.center,
       child: FocusableActionDetector(
-        // focusNode: focusNode,
         autofocus: true,
         shortcuts: {
           if (sendable)
