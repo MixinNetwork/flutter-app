396295dfb9a21a983c333f9707431a592c0dd35c
YeungKC <I@YeungKC.com>
2021-06-24 14:45:35 +0800

    Refactor SlidePage.

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 
 import '../../bloc/bloc_converter.dart';
@@ -6,7 +8,6 @@ import '../../generated/l10n.dart';
 import '../../widgets/automatic_keep_alive_client_widget.dart';
 import '../../widgets/brightness_observer.dart';
 import '../../widgets/empty.dart';
-import '../../widgets/size_policy_row.dart';
 import '../setting/setting_page.dart';
 import 'bloc/slide_category_cubit.dart';
 import 'conversation_page.dart';
@@ -31,18 +32,86 @@ class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+  Widget build(BuildContext context) => LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) =>
+            _HomePage(
+          constraints: constraints,
+        ),
+      );
+}
+
+class HasDrawerValueNotifier extends ValueNotifier<bool> {
+  HasDrawerValueNotifier(bool value) : super(value);
+}
+
+class _HomePage extends HookWidget {
+  const _HomePage({
+    Key? key,
+    required this.constraints,
+  }) : super(key: key);
+
+  final BoxConstraints constraints;
+
+  @override
+  Widget build(BuildContext context) {
+    final animationController = useAnimationController(
+      initialValue: 1,
+      duration: const Duration(milliseconds: 150),
+    );
+
+    final maxWidth = constraints.maxWidth;
+    final clampSlideWidth = (maxWidth - kResponsiveNavigationMinWidth)
+        .clamp(kSlidePageMinWidth, kSlidePageMaxWidth);
+
+    useValueChanged<double, void>(clampSlideWidth, (_, __) {
+      if (animationController.isAnimating) return;
+      if (clampSlideWidth <= kSlidePageMinWidth) {
+        animationController.reverse();
+      } else {
+        animationController.forward();
+      }
+    });
+
+    final hasDrawerValueNotifier =
+        useMemoized(() => HasDrawerValueNotifier(false));
+
+    final animationProgress = useAnimation(animationController);
+
+    final slideWidth = animationProgress * clampSlideWidth;
+
+    final hasDrawer = animationProgress == 0;
+    if (hasDrawerValueNotifier.value != hasDrawer) {
+      hasDrawerValueNotifier.value = hasDrawer;
+    }
+
+    return ChangeNotifierProvider.value(
+      value: hasDrawerValueNotifier,
+      child: Scaffold(
         backgroundColor: BrightnessData.themeOf(context).primary,
-        body: SizePolicyRow(
+        drawer: hasDrawer
+            ? Drawer(
+                child: Scaffold(
+                  backgroundColor: BrightnessData.themeOf(context).primary,
+                  body: const SizedBox(
+                    width: double.infinity,
+                    child: SlidePage(),
+                  ),
+                ),
+              )
+            : null,
+        body: Row(
           children: [
-            SizePolicyData(
-              child: const SlidePage(),
-              minWidth: kSlidePageMinWidth,
-              maxWidth: kSlidePageMaxWidth,
-              sizePolicyOrder: 0,
-            ),
-            SizePolicyData(
-              minWidth: kResponsiveNavigationMinWidth,
+            if (!hasDrawer)
+              SizedBox(
+                width: slideWidth,
+                child: OverflowBox(
+                  alignment: Alignment.centerLeft,
+                  minWidth: kSlidePageMinWidth,
+                  maxWidth: clampSlideWidth,
+                  child: const SlidePage(),
+                ),
+              ),
+            Expanded(
               child: ResponsiveNavigator(
                 switchWidth:
                     kResponsiveNavigationMinWidth + kConversationListWidth,
@@ -70,7 +139,9 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
-      );
+      ),
+    );
+  }
 }
 
 class _CenterPage extends StatelessWidget {
