085f4075a37a0a41c89ba9b3d3f634ba9e937275
YeungKC <I@YeungKC.com>
2021-07-13 12:59:41 +0800

    Fix test.

--- test/uuid_test.dart
+++ test/uuid_test.dart
@@ -1,17 +1,13 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_app/crypto/uuid/uuid.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testGenerateConversationId();
-  debugPrint('Done!');
-}
-
-void testGenerateConversationId() {
-  const a = '80f5130e-a5f5-47e4-80a1-535563687709';
-  const b = '508c75a2-ddce-4858-9e66-cf6d58db15cf';
-  final result = generateConversationId(a, b);
-  final otherResult = generateConversationId(b, a);
-  expect(result, 'a59c170e-02e6-3aa6-8ff1-6cb9350fa8fc');
-  expect(result, otherResult);
+  test('test generate conversationId', () {
+    const a = '80f5130e-a5f5-47e4-80a1-535563687709';
+    const b = '508c75a2-ddce-4858-9e66-cf6d58db15cf';
+    final result = generateConversationId(a, b);
+    final otherResult = generateConversationId(b, a);
+    expect(result, 'a59c170e-02e6-3aa6-8ff1-6cb9350fa8fc');
+    expect(result, otherResult);
+  });
 }
