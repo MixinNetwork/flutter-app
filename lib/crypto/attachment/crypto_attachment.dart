eb4b298e806306313856d3c52564615c07af29f7
YeungKC <I@YeungKC.com>
2021-08-08 19:12:38 +0800

    Remove deprecated code

--- lib/crypto/attachment/crypto_attachment.dart
+++ lib/crypto/attachment/crypto_attachment.dart
@@ -5,66 +5,18 @@ import 'dart:typed_data';
 import 'package:async/async.dart';
 import 'package:convert/convert.dart';
 import 'package:crypto/crypto.dart' as cr;
-import 'package:crypto/crypto.dart';
 import 'package:flutter/foundation.dart';
 import 'package:pointycastle/api.dart';
 import 'package:pointycastle/block/aes_fast.dart';
 import 'package:pointycastle/block/modes/cbc.dart';
 import 'package:pointycastle/padded_block_cipher/padded_block_cipher_impl.dart';
 import 'package:pointycastle/paddings/pkcs7.dart';
-import 'package:tuple/tuple.dart';
 
-import '../../utils/crypto_util.dart';
 import '../../utils/logger.dart';
 
 const int _blockSize = 64 * 1024;
 const int _macSize = 32;
 const int _cbcBlockSize = 16;
-
-class CryptoAttachment {
-  List<int> decryptAttachment(
-      List<int> encryptedBin, List<int> keys, List<int> theirDigest) {
-    final digest = _calculateDigest(encryptedBin);
-    if (!listEquals(digest, theirDigest)) {
-      throw Exception('Invalid digest');
-    }
-
-    final aesKey = keys.sublist(0, 32);
-    final macKey = keys.sublist(32, 64);
-
-    final iv = encryptedBin.sublist(0, 16);
-    final cipherText = encryptedBin.sublist(16, encryptedBin.length - 32);
-    final ivAndCipherText = encryptedBin.sublist(0, encryptedBin.length - 32);
-    final mac =
-        encryptedBin.sublist(encryptedBin.length - 32, encryptedBin.length);
-    final verify = _sign(macKey, ivAndCipherText);
-    if (!listEquals(verify, mac)) {
-      throw Exception('Verification does not pass');
-    }
-    return aesDecrypt(aesKey, iv, cipherText);
-  }
-
-  List<int> _calculateDigest(List<int> data) {
-    final digest = sha256.convert(data);
-    return digest.bytes;
-  }
-
-  List<int> _sign(List<int> key, List<int> data) {
-    final digest = Hmac(sha256, key).convert(data);
-    return digest.bytes;
-  }
-
-  Tuple2<List<int>, List<int>> encryptAttachment(
-      List<int> plainText, List<int> keys, List<int> iv) {
-    final aesKey = keys.sublist(0, 32);
-    final macKey = keys.sublist(32, 64);
-    final ivAndCipherText = aesEncrypt(aesKey, plainText, iv);
-    final mac = Hmac(sha256, macKey).convert(ivAndCipherText).bytes;
-    final encryptedBin = [...ivAndCipherText, ...mac];
-    final digest = _calculateDigest(encryptedBin);
-    return Tuple2(encryptedBin, digest);
-  }
-}
 
 PaddedBlockCipher _getAesCipher(
   CBCBlockCipher cbcCipher,
