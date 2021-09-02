23114afd771390f51c1c33ece8987f01fa87ffad
YeungKC <I@YeungKC.com>
2021-09-02 18:03:49 +0800

    done

---
+++ lib/utils/extension/src/key_event.dart
@@ -0,0 +1,60 @@
+part of '../extension.dart';
+
+const letterKeys = [
+  LogicalKeyboardKey.keyA,
+  LogicalKeyboardKey.keyB,
+  LogicalKeyboardKey.keyC,
+  LogicalKeyboardKey.keyD,
+  LogicalKeyboardKey.keyE,
+  LogicalKeyboardKey.keyF,
+  LogicalKeyboardKey.keyG,
+  LogicalKeyboardKey.keyH,
+  LogicalKeyboardKey.keyI,
+  LogicalKeyboardKey.keyJ,
+  LogicalKeyboardKey.keyK,
+  LogicalKeyboardKey.keyL,
+  LogicalKeyboardKey.keyM,
+  LogicalKeyboardKey.keyN,
+  LogicalKeyboardKey.keyO,
+  LogicalKeyboardKey.keyP,
+  LogicalKeyboardKey.keyQ,
+  LogicalKeyboardKey.keyR,
+  LogicalKeyboardKey.keyS,
+  LogicalKeyboardKey.keyT,
+  LogicalKeyboardKey.keyU,
+  LogicalKeyboardKey.keyV,
+  LogicalKeyboardKey.keyW,
+  LogicalKeyboardKey.keyX,
+  LogicalKeyboardKey.keyY,
+  LogicalKeyboardKey.keyZ,
+];
+
+const numberKeys = [
+  LogicalKeyboardKey.digit0,
+  LogicalKeyboardKey.digit1,
+  LogicalKeyboardKey.digit2,
+  LogicalKeyboardKey.digit3,
+  LogicalKeyboardKey.digit4,
+  LogicalKeyboardKey.digit5,
+  LogicalKeyboardKey.digit6,
+  LogicalKeyboardKey.digit7,
+  LogicalKeyboardKey.digit8,
+  LogicalKeyboardKey.digit9,
+  LogicalKeyboardKey.numpad0,
+  LogicalKeyboardKey.numpad1,
+  LogicalKeyboardKey.numpad2,
+  LogicalKeyboardKey.numpad3,
+  LogicalKeyboardKey.numpad4,
+  LogicalKeyboardKey.numpad5,
+  LogicalKeyboardKey.numpad6,
+  LogicalKeyboardKey.numpad7,
+  LogicalKeyboardKey.numpad8,
+  LogicalKeyboardKey.numpad9,
+];
+
+extension RawKeyEventExtension on RawKeyEvent {
+  bool get firstInputable {
+    if (data.modifiersPressed.isNotEmpty) return false;
+    return letterKeys.contains(logicalKey) || numberKeys.contains(logicalKey);
+  }
+}
