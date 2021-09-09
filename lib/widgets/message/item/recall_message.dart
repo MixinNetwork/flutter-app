2bca270d4394707b47147c3e0f08386c002fc0ea
boyan <17426470+boyan01@users.noreply.github.com>
2021-09-09 16:21:28 +0800

    improve update performance.

--- lib/widgets/message/item/recall_message.dart
+++ lib/widgets/message/item/recall_message.dart
@@ -2,18 +2,20 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../../constants/resources.dart';
 import '../../../db/mixin_database.dart' hide Offset, Message;
 import '../../../ui/home/bloc/recall_message_bloc.dart';
 import '../../../utils/extension/extension.dart';
+import '../../../utils/hook.dart';
 import '../message.dart';
 import '../message_bubble.dart';
 import '../message_datetime_and_status.dart';
 import '../message_layout.dart';
 
-class RecallMessage extends StatelessWidget {
+class RecallMessage extends HookWidget {
   const RecallMessage({
     Key? key,
     required this.showNip,
@@ -27,9 +29,11 @@ class RecallMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final recalledText =
-        context.watch<RecallMessageReeditCubit>().state[message.messageId];
-
+    final recalledText = useBlocStateConverter<RecallMessageReeditCubit,
+        Map<String, String>, String?>(
+      converter: (state) => state[message.messageId],
+      keys: [message.messageId],
+    );
     final content = Row(
       mainAxisSize: MainAxisSize.min,
       children: [
