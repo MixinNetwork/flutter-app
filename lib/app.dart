362802438d1c005e9c09b4d83d893faedce8e78f
YeungKC <I@YeungKC.com>
2021-10-14 17:35:43 +0800

    Auto clean selection.

--- lib/app.dart
+++ lib/app.dart
@@ -31,9 +31,9 @@ import 'utils/extension/extension.dart';
 import 'utils/hook.dart';
 import 'utils/logger.dart';
 import 'utils/system/tray.dart';
+import 'widgets/auto_focus.dart';
 import 'widgets/brightness_observer.dart';
 import 'widgets/default_text_editing_focusable_action_detector.dart';
-import 'widgets/desktop_lifecycle_auto_fcous.dart';
 import 'widgets/message/item/text/mention_builder.dart';
 import 'widgets/window/move_window.dart';
 import 'widgets/window/window_shortcuts.dart';
@@ -45,7 +45,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     precacheImage(
         const AssetImage(Resources.assetsImagesChatBackgroundPng), context);
-    return DesktopLifecycleAutoFocus(
+    return AutoFocus(
       child: DefaultTextEditingFocusableActionDetector(
         child: MultiBlocProvider(
           providers: [
---
+++ lib/widgets/auto_focus.dart
@@ -0,0 +1,76 @@
+import 'package:desktop_lifecycle/desktop_lifecycle.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class AutoFocus extends HookWidget {
+  const AutoFocus({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    useDesktopLifecycleAutoFocus();
+    useEditableTextAutoCleanSelection();
+
+    return child;
+  }
+}
+
+void useDesktopLifecycleAutoFocus() {
+  final ref = useRef<FocusNode?>(null);
+  useEffect(() {
+    void onListen() {
+      if (DesktopLifecycle.instance.isActive.value) {
+        ref.value?.requestFocus();
+        ref.value = null;
+        return;
+      }
+      ref.value = FocusManager.instance.primaryFocus;
+      ref.value?.unfocus();
+    }
+
+    DesktopLifecycle.instance.isActive.addListener(onListen);
+    return () {
+      DesktopLifecycle.instance.isActive.removeListener(onListen);
+      ref.value = null;
+    };
+  });
+}
+
+void _cleanSelection(BuildContext? context) {
+  if (context is! StatefulElement || context.widget is! EditableText) return;
+  final editableText = context.widget as EditableText;
+
+  final controller = editableText.controller;
+  final selection = controller.selection;
+  controller.selection = selection.copyWith(
+    baseOffset: selection.baseOffset,
+    extentOffset: selection.baseOffset,
+    isDirectional: true,
+  );
+}
+
+void useEditableTextAutoCleanSelection() {
+  final previousFocusRef = useRef<FocusNode?>(null);
+  useEffect(() {
+    void listener() {
+      if (!DesktopLifecycle.instance.isActive.value) return;
+
+      final primaryFocus = FocusManager.instance.primaryFocus;
+      if (previousFocusRef.value == primaryFocus) return;
+
+      _cleanSelection(previousFocusRef.value?.context);
+
+      previousFocusRef.value = primaryFocus;
+    }
+
+    FocusManager.instance.addListener(listener);
+    return () {
+      FocusManager.instance.removeListener(listener);
+      previousFocusRef.value = null;
+    };
+  });
+}
