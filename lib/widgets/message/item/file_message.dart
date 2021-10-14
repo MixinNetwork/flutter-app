210aa2be6187d2f597f90b975f6215eac8b69236
boyan <17426470+boyan01@users.noreply.github.com>
2021-10-14 12:57:12 +0800

    support open files on android/ios

--- lib/widgets/message/item/file_message.dart
+++ lib/widgets/message/item/file_message.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:filesize/filesize.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:mime/mime.dart';
 import 'package:mixin_bot_sdk_dart/mixin_bot_sdk_dart.dart';
+import 'package:open_file/open_file.dart';
 
 import '../../../constants/brightness_theme_data.dart';
 import '../../../enum/media_status.dart';
@@ -58,12 +61,15 @@ class FileMessage extends HookWidget {
           } else if (message.mediaStatus == MediaStatus.done &&
               message.mediaUrl != null) {
             if (message.mediaUrl?.isEmpty ?? true) return;
-            await saveFileToSystem(
-              context,
-              context.accountServer
-                  .convertMessageAbsolutePath(message, isTranscriptPage),
-              suggestName: message.mediaName,
-            );
+
+            final path = context.accountServer
+                .convertMessageAbsolutePath(message, isTranscriptPage);
+            if (Platform.isAndroid || Platform.isIOS) {
+              await OpenFile.open(path);
+            } else {
+              await saveFileToSystem(context, path,
+                  suggestName: message.mediaName);
+            }
           } else if (message.mediaStatus == MediaStatus.pending) {
             await context.accountServer
                 .cancelProgressAttachmentJob(message.messageId);
