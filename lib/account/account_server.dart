3f525159bd603c6d09a91a5f7121fd62b5f9318f
Crossle Song <crosslesong@gmail.com>
2021-11-17 21:55:15 +0800

    Fix format

--- lib/account/account_server.dart
+++ lib/account/account_server.dart
@@ -96,7 +96,8 @@ class AccountServer {
               final serverTime = int.tryParse(
                   e.response?.headers.value('x-server-time') ?? '');
               if (serverTime != null) {
-                final time = DateTime.fromMicrosecondsSinceEpoch(serverTime ~/ 1000);
+                final time =
+                    DateTime.fromMicrosecondsSinceEpoch(serverTime ~/ 1000);
                 final difference = time.difference(DateTime.now());
                 if (difference.inMinutes > 5) {
                   blaze.waitSyncTime();
