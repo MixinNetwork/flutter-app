1fc91782f10781ba3705ae89b823d084fa721613
boyan <17426470+boyan01@users.noreply.github.com>
2022-01-06 12:02:29 +0800

    fix click tray icon restore minimized window on Windows.

--- lib/utils/system/tray.dart
+++ lib/utils/system/tray.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'dart:io';
 
 import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:ffi/ffi.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:path/path.dart' as p;
 import 'package:system_tray/system_tray.dart';
-import 'package:win_toast/win_toast.dart';
+import 'package:win32/win32.dart';
 
 import '../../constants/resources.dart';
 import '../extension/extension.dart';
@@ -94,9 +96,21 @@ Future<void> _initSystemTray() async {
     switch (eventName) {
       case 'leftMouseUp':
         if (Platform.isWindows) {
-          WinToast.instance().bringWindowToFront();
+          final handle = appWindow.handle;
+          assert(handle != null, 'handle is null');
+          if (handle != null) {
+            final placement =
+                calloc.allocate<WINDOWPLACEMENT>(sizeOf<WINDOWPLACEMENT>());
+            GetWindowPlacement(handle, placement);
+            if (placement.ref.showCmd == SW_SHOWMINIMIZED) {
+              ShowWindow(handle, SW_RESTORE);
+            }
+            SetForegroundWindow(handle);
+            appWindow.show();
+          }
+        } else {
+          appWindow.show();
         }
-        appWindow.show();
         break;
       default:
         break;
