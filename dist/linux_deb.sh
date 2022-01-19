29bc4fba9d9e85bcabef0ef61b9e6507fbb6d924
boyan01 <17426470+boyan01@users.noreply.github.com>
2022-01-19 20:13:58 +0800

    support build deb package.

---
+++ dist/linux_deb.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+app_name="mixin_desktop"
+current_dir=$(dirname "$0")
+
+project_dir="${current_dir}/.."
+package_dir="${project_dir}/build/deb_package"
+
+rm -rf "${package_dir}"
+
+cp -fr "$current_dir/deb/." "$package_dir"
+mkdir -p "$package_dir/usr/lib/$app_name"
+cp -fr "$project_dir/build/linux/x64/release/bundle/." \
+  "$package_dir/usr/lib/$app_name"
+cp "/usr/lib/x86_64-linux-gnu/libsqlite3.so.0" \
+  "$package_dir/usr/lib/$app_name/lib/libsqlite3.so"
+
+mkdir -p "$package_dir/usr/bin"
+cd "$package_dir/usr/bin" || exit
+ln -s "../lib/$app_name/$app_name" "$app_name"
+cd "$current_dir" || exit
+
+dpkg-deb --build --root-owner-group "$package_dir" "$project_dir/build/${app_name}_amd64.deb"
