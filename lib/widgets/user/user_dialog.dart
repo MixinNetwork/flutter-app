6067cecc658af9c79b59ba6cc6bed4e9fcdf24e0
YeungKC <I@YeungKC.com>
2022-01-21 21:06:40 +0800

    Anonymous user dialog (#491)
    * Anonymous user dialog
    
    * analysis

--- lib/widgets/user/user_dialog.dart
+++ lib/widgets/user/user_dialog.dart
@@ -148,59 +148,68 @@ class _UserProfileBody extends StatelessWidget {
   final bool isSelf;
 
   @override
-  Widget build(BuildContext context) => AnimatedSize(
-        duration: const Duration(milliseconds: 150),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            AvatarWidget(
-              size: 90,
-              avatarUrl: user.avatarUrl,
-              userId: user.userId,
-              name: user.fullName ?? '',
-            ),
-            const SizedBox(height: 8),
-            Row(
+  Widget build(BuildContext context) {
+    final anonymous = user.identityNumber == '0';
+    return AnimatedSize(
+      duration: const Duration(milliseconds: 150),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AvatarWidget(
+            size: 90,
+            avatarUrl: anonymous ? null : user.avatarUrl,
+            userId: user.userId,
+            name: user.fullName ?? '',
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(
+                child: SelectableText(
+                  user.fullName ?? '',
+                  style: TextStyle(
+                    color: context.theme.text,
+                    fontSize: 16,
+                    height: 1,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              VerifiedOrBotWidget(
+                verified: user.isVerified,
+                isBot: !anonymous && user.appId != null,
+              )
+            ],
+          ),
+          if (!anonymous)
+            Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Flexible(
-                  child: SelectableText(
-                    user.fullName ?? '',
-                    style: TextStyle(
-                      color: context.theme.text,
-                      fontSize: 16,
-                      height: 1,
-                    ),
-                    textAlign: TextAlign.center,
+                const SizedBox(height: 4),
+                SelectableText(
+                  context.l10n.contactMixinId(user.identityNumber),
+                  style: TextStyle(
+                    color: context.theme.secondaryText,
+                    fontSize: 12,
                   ),
                 ),
-                VerifiedOrBotWidget(
-                  verified: user.isVerified,
-                  isBot: user.appId != null,
-                )
+                if (user.isStranger)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: _AddToContactsButton(user: user),
+                  ),
+                const SizedBox(height: 20),
+                _BioText(biography: user.biography ?? ''),
+                const SizedBox(height: 24),
+                _UserProfileButtonBar(user: user),
               ],
             ),
-            const SizedBox(height: 4),
-            SelectableText(
-              context.l10n.contactMixinId(user.identityNumber),
-              style: TextStyle(
-                color: context.theme.secondaryText,
-                fontSize: 12,
-              ),
-            ),
-            if (user.isStranger)
-              Padding(
-                padding: const EdgeInsets.only(top: 8),
-                child: _AddToContactsButton(user: user),
-              ),
-            const SizedBox(height: 20),
-            _BioText(biography: user.biography ?? ''),
-            const SizedBox(height: 24),
-            _UserProfileButtonBar(user: user),
-            const SizedBox(height: 56),
-          ],
-        ),
-      );
+          const SizedBox(height: 56),
+        ],
+      ),
+    );
+  }
 }
 
 class _BioText extends StatelessWidget {
