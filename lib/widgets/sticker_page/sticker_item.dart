8f0cf2ce7e5671c682b7625d529ef99c2c42ee69
YeungKC <I@YeungKC.com>
2022-01-27 17:51:45 +0800

    Stop image animation on inactive

--- lib/widgets/sticker_page/sticker_item.dart
+++ lib/widgets/sticker_page/sticker_item.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:lottie/lottie.dart';
 
+import '../../utils/app_lifecycle.dart';
 import '../cache_image.dart';
 
-class StickerItem extends StatelessWidget {
+class StickerItem extends HookWidget {
   const StickerItem({
     Key? key,
     required this.assetUrl,
@@ -22,18 +24,45 @@ class StickerItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     late Widget child;
-    if (assetType == 'json') {
+    final playing = useState(false);
+    final isJson = useMemoized(() => assetType == 'json', [assetType]);
+    final controller = useAnimationController();
+
+    final listener = useCallback(() {
+      if (isJson && controller.duration == null) return;
+      if (isAppActive) {
+        if (isJson) controller.repeat();
+        playing.value = true;
+      } else {
+        if (isJson) controller.stop();
+        playing.value = false;
+      }
+    }, [controller]);
+
+    useEffect(() {
+      listener();
+      appActiveListener.addListener(listener);
+      return () => appActiveListener.removeListener(listener);
+    }, [controller, appActiveListener]);
+
+    if (isJson) {
       child = Lottie.network(
         assetUrl,
+        controller: controller,
         height: height,
         width: width,
         fit: BoxFit.cover,
+        onLoaded: (composition) {
+          controller.duration = composition.duration;
+          listener();
+        },
       );
     } else {
       child = CacheImage(
         assetUrl,
         height: height,
         width: width,
+        controller: playing,
         placeholder: () => placeholder ?? const SizedBox(),
       );
     }
