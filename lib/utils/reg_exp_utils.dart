43a0c7e7d660800a899c3926056e12fcb4226eba
YeungKC <I@YeungKC.com>
2022-03-11 17:50:12 +0800

    Improve bot number regexp (#555)

--- lib/utils/reg_exp_utils.dart
+++ lib/utils/reg_exp_utils.dart
@@ -1,5 +1,5 @@
 final mentionRegExp = RegExp(r'@(\S*)$');
 final mentionNumberRegExp = RegExp(r'@(\d{4,})');
 final uriRegExp = RegExp(r'[a-zA-z]+://[^\s].*?((?=["\s，）)(（。：])|$)');
-final botNumberStartRegExp = RegExp(r'^\s*@(700\d*)');
-final botNumberRegExp = RegExp(r'7000\d{6}');
+final botNumberStartRegExp = RegExp(r'(?<=^\s*@)700\d{6}(?=$|\D)');
+final botNumberRegExp = RegExp(r'(?<=^|\D)7000\d{6}(?=$|\D)');
