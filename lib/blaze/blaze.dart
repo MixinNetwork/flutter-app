aca5eff3810e53fee6ad7a26cc846d9da9e2c81d
YeungKC <I@YeungKC.com>
2022-03-15 14:09:21 +0800

    Database support multi read. (#552)
    * Database support multi read.
    
    * connectedState
    
    * ensure database unlock
    
    * format
    
    * driftRuntimeOptions.dontWarnAboutMultipleDatabases = true
    
    * format
    
    * remove cache isolate
    
    * refactor
    
    * update drift
    
    * update drift

--- lib/blaze/blaze.dart
+++ lib/blaze/blaze.dart
@@ -16,7 +16,7 @@ import '../db/extension/job.dart';
 import '../db/mixin_database.dart';
 import '../utils/extension/extension.dart';
 import '../utils/logger.dart';
-import '../workers/message_woker_isolate.dart';
+import '../workers/message_worker_isolate.dart';
 import 'blaze_message.dart';
 import 'blaze_message_param_session.dart';
 
@@ -72,6 +72,7 @@ class Blaze {
 
     _connectedStateBehaviorSubject.value = state;
 
+    i('connectedState: $state, ${StackTrace.current}');
     if (state == ConnectedState.connected) {
       _refreshOffset();
     }
