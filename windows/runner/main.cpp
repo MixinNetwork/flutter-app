3b37034082c77dab6b5eb3f4423e2365d6377285
boyan01 <17426470+boyan01@users.noreply.github.com>
2022-03-21 13:39:11 +0800

    fix FindWindow

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -32,7 +32,7 @@ class CSingleInstance {
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
-  HWND hwnd = ::FindWindow(L"FLUTTER_RUNNER_WIN32_WINDOW", L"protocol_handler_example");
+  HWND hwnd = ::FindWindow(L"FLUTTER_RUNNER_WIN32_WINDOW", L"Mixin");
   if (hwnd != NULL) {
     DispatchToProtocolHandler(hwnd);
 
