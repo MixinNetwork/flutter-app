c3eeabe8be943a5e40b7af925d05a87a9d87b255
Bin <17426470+boyan01@users.noreply.github.com>
2022-04-06 13:21:59 +0800

    fix warnings on latest master channel (#582)
    * fix warnings on latest master channel
    
    * fix
    
    * fix

--- third_party/system_tray/lib/src/tray.dart
+++ third_party/system_tray/lib/src/tray.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide MenuItem;
 import 'package:flutter/services.dart';
 import 'package:path/path.dart' show dirname, joinAll;
 
