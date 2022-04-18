c5a668edd8e6eb9c0ebd24b5d2ce5c281468f9ba
Bin <17426470+boyan01@users.noreply.github.com>
2022-04-18 12:58:13 +0800

    display video message in share media page (#596)

--- lib/widgets/message/item/video_message.dart
+++ lib/widgets/message/item/video_message.dart
@@ -29,24 +29,10 @@ class VideoMessageWidget extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isCurrentUser = useIsCurrentUser();
-    final isTranscriptPage = useIsTranscriptPage();
-
     final mediaWidth =
         useMessageConverter(converter: (state) => state.mediaWidth);
     final mediaHeight =
         useMessageConverter(converter: (state) => state.mediaHeight);
-
-    final thumbImage =
-        useMessageConverter(converter: (state) => state.thumbImage);
-    final thumbUrl = useMessageConverter(converter: (state) => state.thumbUrl);
-    final mediaStatus =
-        useMessageConverter(converter: (state) => state.mediaStatus);
-    final relationship =
-        useMessageConverter(converter: (state) => state.relationship);
-    final mediaUrl = useMessageConverter(converter: (state) => state.mediaUrl);
-    final isVideo =
-        useMessageConverter(converter: (state) => state.type.isVideo);
 
     return LayoutBuilder(
       builder: (context, boxConstraints) {
@@ -61,127 +47,182 @@ class VideoMessageWidget extends HookWidget {
           padding: EdgeInsets.zero,
           includeNip: true,
           clip: true,
-          child: InteractiveDecoratedBox(
-            onTap: () {
-              final message = context.message;
-              if (message.mediaStatus == MediaStatus.canceled) {
-                if (message.relationship == UserRelationship.me &&
-                    message.mediaUrl?.isNotEmpty == true) {
-                  context.accountServer.reUploadAttachment(message);
-                } else {
-                  context.accountServer.downloadAttachment(message.messageId);
-                }
-              } else if (message.mediaStatus == MediaStatus.done &&
-                  message.mediaUrl != null) {
-                final path = context.accountServer
-                    .convertMessageAbsolutePath(message, isTranscriptPage);
-                if (Platform.isIOS || Platform.isAndroid) {
-                  OpenFile.open(path);
-                } else {
-                  openUri(context, Uri.file(path).toString());
-                }
-              } else if (message.mediaStatus == MediaStatus.pending) {
-                context.accountServer
-                    .cancelProgressAttachmentJob(message.messageId);
-              } else if (message.type.isLive && message.mediaUrl != null) {
-                launch(message.mediaUrl!);
-              }
-            },
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: SizedBox(
-                height: height,
-                width: width,
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    if (thumbImage != null)
-                      ImageByBlurHashOrBase64(imageData: thumbImage),
-                    if (thumbUrl != null) CacheImage(thumbUrl),
-                    Center(
-                      child: Builder(
-                        builder: (BuildContext context) {
-                          switch (mediaStatus) {
-                            case MediaStatus.canceled:
-                              if (relationship == UserRelationship.me &&
-                                  mediaUrl?.isNotEmpty == true) {
-                                return const StatusUpload();
-                              } else {
-                                return const StatusDownload();
-                              }
-                            case MediaStatus.pending:
-                              return const StatusPending();
-                            case MediaStatus.expired:
-                              return const StatusWarning();
-                            case MediaStatus.done:
-                            case MediaStatus.read:
-                            case null:
-                              return SvgPicture.asset(
-                                Resources.assetsImagesPlaySvg,
-                                width: 38,
-                                height: 38,
-                              );
-                          }
-                        },
-                      ),
-                    ),
-                    if (isVideo)
-                      HookBuilder(builder: (context) {
-                        final durationText = useMessageConverter(
-                          converter: (state) => Duration(
-                                  milliseconds:
-                                      int.tryParse(state.mediaDuration ?? '') ??
-                                          0)
-                              .asMinutesSeconds,
-                        );
-
-                        return Positioned(
-                          top: 6,
-                          left: isCurrentUser ? 6 : 14,
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(
-                              color: const Color.fromRGBO(0, 0, 0, 0.3),
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(4),
-                              child: Text(
-                                durationText,
-                                style: const TextStyle(
-                                  fontSize: MessageItemWidget.tertiaryFontSize,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        );
-                      }),
-                    Positioned(
-                      bottom: 4,
-                      right: isCurrentUser ? 12 : 4,
-                      child: const DecoratedBox(
-                        decoration: ShapeDecoration(
-                          color: Color.fromRGBO(0, 0, 0, 0.3),
-                          shape: StadiumBorder(),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 3,
-                            horizontal: 5,
-                          ),
-                          child: MessageDatetimeAndStatus(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+          child: SizedBox(
+            width: width,
+            height: height,
+            child: const MessageVideo(),
           ),
         );
       },
+    );
+  }
+}
+
+class MessageVideo extends HookWidget {
+  const MessageVideo({
+    Key? key,
+    this.overlay,
+  }) : super(key: key);
+
+  final Widget? overlay;
+
+  @override
+  Widget build(BuildContext context) {
+    final isCurrentUser = useIsCurrentUser();
+    final isTranscriptPage = useIsTranscriptPage();
+
+    final thumbImage =
+        useMessageConverter(converter: (state) => state.thumbImage);
+    final thumbUrl = useMessageConverter(converter: (state) => state.thumbUrl);
+
+    return InteractiveDecoratedBox(
+      onTap: () {
+        final message = context.message;
+        if (message.mediaStatus == MediaStatus.canceled) {
+          if (message.relationship == UserRelationship.me &&
+              message.mediaUrl?.isNotEmpty == true) {
+            context.accountServer.reUploadAttachment(message);
+          } else {
+            context.accountServer.downloadAttachment(message.messageId);
+          }
+        } else if (message.mediaStatus == MediaStatus.done &&
+            message.mediaUrl != null) {
+          final path = context.accountServer
+              .convertMessageAbsolutePath(message, isTranscriptPage);
+          if (Platform.isIOS || Platform.isAndroid) {
+            OpenFile.open(path);
+          } else {
+            openUri(context, Uri.file(path).toString());
+          }
+        } else if (message.mediaStatus == MediaStatus.pending) {
+          context.accountServer.cancelProgressAttachmentJob(message.messageId);
+        } else if (message.type.isLive && message.mediaUrl != null) {
+          launch(message.mediaUrl!);
+        }
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            if (thumbImage != null)
+              ImageByBlurHashOrBase64(imageData: thumbImage),
+            if (thumbUrl != null) CacheImage(thumbUrl),
+            overlay ?? _VideoMessageOverlayInfo(isCurrentUser: isCurrentUser),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class VideoMessageMediaStatusWidget extends HookWidget {
+  const VideoMessageMediaStatusWidget({
+    Key? key,
+    this.done,
+  }) : super(key: key);
+
+  final Widget? done;
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaStatus =
+        useMessageConverter(converter: (state) => state.mediaStatus);
+    final relationship =
+        useMessageConverter(converter: (state) => state.relationship);
+    final mediaUrl = useMessageConverter(converter: (state) => state.mediaUrl);
+    switch (mediaStatus) {
+      case MediaStatus.canceled:
+        if (relationship == UserRelationship.me &&
+            mediaUrl?.isNotEmpty == true) {
+          return const StatusUpload();
+        } else {
+          return const StatusDownload();
+        }
+      case MediaStatus.pending:
+        return const StatusPending();
+      case MediaStatus.expired:
+        return const StatusWarning();
+      case MediaStatus.done:
+      case MediaStatus.read:
+      case null:
+        return done ??
+            SvgPicture.asset(
+              Resources.assetsImagesPlaySvg,
+              width: 38,
+              height: 38,
+            );
+    }
+  }
+}
+
+class _VideoMessageOverlayInfo extends HookWidget {
+  const _VideoMessageOverlayInfo({
+    Key? key,
+    required this.isCurrentUser,
+  }) : super(key: key);
+
+  final bool isCurrentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    final isVideo =
+        useMessageConverter(converter: (state) => state.type.isVideo);
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        const Center(
+          child: VideoMessageMediaStatusWidget(),
+        ),
+        if (isVideo)
+          HookBuilder(builder: (context) {
+            final durationText = useMessageConverter(
+              converter: (state) => Duration(
+                      milliseconds:
+                          int.tryParse(state.mediaDuration ?? '') ?? 0)
+                  .asMinutesSeconds,
+            );
+            return Positioned(
+              top: 6,
+              left: isCurrentUser ? 6 : 14,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(0, 0, 0, 0.3),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: Text(
+                    durationText,
+                    style: const TextStyle(
+                      fontSize: MessageItemWidget.tertiaryFontSize,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }),
+        Positioned(
+          bottom: 4,
+          right: isCurrentUser ? 12 : 4,
+          child: const DecoratedBox(
+            decoration: ShapeDecoration(
+              color: Color.fromRGBO(0, 0, 0, 0.3),
+              shape: StadiumBorder(),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 3,
+                horizontal: 5,
+              ),
+              child: MessageDatetimeAndStatus(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
