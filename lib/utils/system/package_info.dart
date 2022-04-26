15dca20bb54002f2a22e5db9c6895b934465e52f
Crossle Song <crosslesong@gmail.com>
2022-04-26 10:39:11 +0400

    Bump windows version to 0.35.2

--- lib/utils/system/package_info.dart
+++ lib/utils/system/package_info.dart
@@ -5,7 +5,7 @@ import 'package:package_info_plus/package_info_plus.dart';
 
 import '../logger.dart';
 
-const kMixinVersion = '0.35.0';
+const kMixinVersion = '0.35.2';
 
 final _packageInfo = PackageInfo(
   appName: 'mixin',
