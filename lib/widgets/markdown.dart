a55a537c4a94fd363eadeb77978d9718faa8a68c
YeungKC <I@YeungKC.com>
2022-07-22 02:28:35 +0800

    test

--- lib/widgets/markdown.dart
+++ lib/widgets/markdown.dart
@@ -4,6 +4,33 @@ import 'package:markdown_widget/markdown_widget.dart';
 import '../utils/uri_utils.dart';
 import 'cache_image.dart';
 
+StyleConfig buildMarkdownStyleConfig(BuildContext context, bool darkMode) =>
+    StyleConfig(
+      markdownTheme:
+          darkMode ? MarkdownTheme.darkTheme : MarkdownTheme.lightTheme,
+      imgBuilder: (url, attributes) {
+        double? width;
+        double? height;
+        if (attributes['width'] != null) {
+          width = double.parse(attributes['width']!);
+        }
+        if (attributes['height'] != null) {
+          height = double.parse(attributes['height']!);
+        }
+        final imageUrl = url;
+
+        return CacheImage(
+          imageUrl,
+          width: width,
+          height: height,
+        );
+      },
+      pConfig: PConfig(onLinkTap: (href) {
+        if (href?.isEmpty ?? true) return;
+        openUri(context, href!);
+      }),
+    );
+
 class Markdown extends StatelessWidget {
   const Markdown({
     super.key,
@@ -23,30 +50,6 @@ class Markdown extends StatelessWidget {
         data: data,
         padding: padding,
         physics: physics,
-        styleConfig: StyleConfig(
-          markdownTheme:
-              darkMode ? MarkdownTheme.darkTheme : MarkdownTheme.lightTheme,
-          imgBuilder: (url, attributes) {
-            double? width;
-            double? height;
-            if (attributes['width'] != null) {
-              width = double.parse(attributes['width']!);
-            }
-            if (attributes['height'] != null) {
-              height = double.parse(attributes['height']!);
-            }
-            final imageUrl = url;
-
-            return CacheImage(
-              imageUrl,
-              width: width,
-              height: height,
-            );
-          },
-          pConfig: PConfig(onLinkTap: (href) {
-            if (href?.isEmpty ?? true) return;
-            openUri(context, href!);
-          }),
-        ),
+        styleConfig: buildMarkdownStyleConfig(context, darkMode),
       );
 }
