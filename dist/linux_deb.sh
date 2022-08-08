230abd1afdbd04d1b67d10eb32d81831a9759205
boyan01 <17426470+boyan01@users.noreply.github.com>
2022-08-08 19:30:41 +0800

    auto update deb control file with pubspec.yaml version

--- dist/linux_deb.sh
+++ dist/linux_deb.sh
@@ -8,6 +8,22 @@ package_dir="${project_dir}/build/deb_package"
 
 rm -rf "${package_dir}"
 
+control_file="${current_dir}/deb/DEBIAN/control"
+
+# read version from pubspec.yaml
+version=$(cat "${project_dir}/pubspec.yaml" | grep "^version:" | awk '{print $2}' | tr -d '"')
+version=$(echo "${version}" | sed 's/+.*//')
+
+# check version only contains numbers and dots
+if ! [[ "${version}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Error: Invalid version format: ${version}"
+  exit 1
+fi
+
+echo "update control file with version: ${version}"
+# update control file version
+sed -i "s/Version:.*/Version: ${version}/g" "${control_file}"
+
 cp -fr "$current_dir/deb/." "$package_dir"
 mkdir -p "$package_dir/usr/lib/$app_name"
 cp -fr "$project_dir/build/linux/x64/release/bundle/." \
