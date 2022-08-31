bdf9311ec95307ba4e226d8612195ac1d1d5d20b
boyan <17426470+boyan01@users.noreply.github.com>
2022-08-31 09:25:21 +0800

    fix format

--- lib/ui/home/chat/files_preview.dart
+++ lib/ui/home/chat/files_preview.dart
@@ -241,7 +241,8 @@ class _FilesPreviewDialog extends HookWidget {
                       },
                       style: ElevatedButton.styleFrom(
                         padding: const EdgeInsets.only(
-                            left: 32, top: 18, bottom: 18, right: 32), backgroundColor: context.theme.accent,
+                            left: 32, top: 18, bottom: 18, right: 32),
+                        backgroundColor: context.theme.accent,
                       ),
                       child: Text(context.l10n.send.toUpperCase()),
                     ),
