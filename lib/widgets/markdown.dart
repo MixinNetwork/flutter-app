b07b3610cabc7bf14859c7a2354ebad4c3312457
boyan <17426470+boyan01@users.noreply.github.com>
2022-09-22 14:33:13 +0800

    Revert "Bump markdown_widget from 1.2.9 to 1.3.0+1 (#867)"
    This reverts commit 2420415a0cf4a0af9bfca60046446cd553fbe3ab.

--- lib/widgets/markdown.dart
+++ lib/widgets/markdown.dart
@@ -30,6 +30,7 @@ StyleConfig buildMarkdownStyleConfig(BuildContext context, bool darkMode) =>
           if (href?.isEmpty ?? true) return;
           openUri(context, href!);
         },
+        selectable: false,
       ),
       olConfig: OlConfig(selectable: false),
       ulConfig: UlConfig(selectable: false),
