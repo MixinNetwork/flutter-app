3722aecdaf3715f2c66f8497e6b9ed0e7d712d62
Crossle Song <crosslesong@gmail.com>
2022-11-01 15:30:47 +0400

    use flutter master build

--- lib/widgets/message/item/post_message.dart
+++ lib/widgets/message/item/post_message.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:markdown_widget/markdown_widget.dart';
