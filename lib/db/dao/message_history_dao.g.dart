c6009e8c0667b3fb34b79c15fc3990938b74dbf4
YeungKC <I@YeungKC.com>
2023-01-19 17:24:14 +0800

    Upgrade drift (#1015)

--- lib/db/dao/message_history_dao.g.dart
+++ lib/db/dao/message_history_dao.g.dart
@@ -2,8 +2,5 @@
 
 part of 'message_history_dao.dart';
 
-// **************************************************************************
-// DaoGenerator
-// **************************************************************************
-
+// ignore_for_file: type=lint
 mixin _$MessageHistoryDaoMixin on DatabaseAccessor<MixinDatabase> {}
