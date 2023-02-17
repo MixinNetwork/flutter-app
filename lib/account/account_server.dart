7dfdf65cc469c92eb72c932379ab9b8ec6cedda8
YeungKC <I@YeungKC.com>
2023-02-17 13:27:22 +0900

    format

--- lib/account/account_server.dart
+++ lib/account/account_server.dart
@@ -152,7 +152,8 @@ class AccountServer {
   }
 
   Future<void> _initDatabase() async {
-    database = Database(await db.connectToDatabase(identityNumber, fromMainIsolate: true));
+    database = Database(
+        await db.connectToDatabase(identityNumber, fromMainIsolate: true));
 
     attachmentUtil = AttachmentUtil.init(
       client,
