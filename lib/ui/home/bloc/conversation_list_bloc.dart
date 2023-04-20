6b920c681d514d05d11789df316b8b733517d56d
YeungKC <I@YeungKC.com>
2023-04-20 15:01:00 +0900

    perf: Increase event stream throttle time in ConversationListBloc (#1143)
    * perf: Increase event stream throttle time in ConversationListBloc
    
    - Increase the throttle time for event streams in ConversationListBloc
    - Improve performance and reduce excessive event triggers
    - Refactor ConversationListBloc for better readability and maintenance
    
    * format

--- lib/ui/home/bloc/conversation_list_bloc.dart
+++ lib/ui/home/bloc/conversation_list_bloc.dart
@@ -74,9 +74,7 @@ class ConversationListBloc extends Cubit<PagingState<ConversationItem>>
     DataBaseEventBus.instance.updateConversationIdStream,
     DataBaseEventBus.instance.insertOrReplaceMessageIdsStream,
     DataBaseEventBus.instance.updateMessageMentionStream,
-  ])
-      .throttleTime(kDefaultThrottleDuration ~/ 10, trailing: true)
-      .asBroadcastStream();
+  ]).throttleTime(kDefaultThrottleDuration, trailing: true).asBroadcastStream();
 
   void _switchBloc(
     SlideCategoryState state,
