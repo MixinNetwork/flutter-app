e372cbff7a00cc0964afb7347db8c0122c3b7914
YeungKC <I@YeungKC.com>
2023-05-06 15:45:30 +0900

    refactor: Improve Regular Expression Validity and Testing
    - Update regular expressions for URI, bot numbers and email addresses
    - Upgrade package_info_plus and path_provider_foundation dependencies
    - Update COCOAPODS to version `1.12.1`
    - Add comprehensive tests for URI and email regular expressions in `regex_test.dart`

--- lib/utils/reg_exp_utils.dart
+++ lib/utils/reg_exp_utils.dart
@@ -1,8 +1,8 @@
 final mentionRegExp = RegExp(r'@(\S*)$');
 final mentionNumberRegExp = RegExp(r'@(\d{4,})');
 // ignore: avoid-non-ascii-symbols
-final uriRegExp = RegExp(r'[a-zA-z]+://\S.*?((?=["\s，）)(（。：])|$)');
-final botNumberRegExp = RegExp(r'(?<=^|\D)7000\d{6}(?=$|\D)');
+final uriRegExp = RegExp(r'\b([a-zA-z+]+:(?://)?[\w-]+(?:\.[\w-]+)*(?:[\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)\b/?');
+final botNumberRegExp = RegExp(r'(?<!\d)7000\d{6}(?!\d)');
 final mailRegExp = RegExp(
-    r'[a-zA-Z\d_.%+\-]{1,256}@[a-zA-Z\d][a-zA-Z\d\-]{0,64}(\.[a-zA-Z\d][a-zA-Z\d\-]{0,25})+');
+    r'\b[\w.%+-]+@[\w.-]+\.[A-Za-z]{2,}\b');
 final numberRegExp = RegExp(r'^\d{4,}$');
