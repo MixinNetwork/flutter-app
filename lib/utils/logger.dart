ebf991add319b94b93481686db3feb4dc160599a
Bin <17426470+boyan01@users.noreply.github.com>
2023-05-17 20:26:28 +0800

    support long click user avatar in conversation to send log file (#1207)

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,1 +1,55 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:archive/archive_io.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:mixin_logger/mixin_logger.dart';
+import 'package:rxdart/rxdart.dart';
+
+import '../ui/home/bloc/conversation_cubit.dart';
+import '../ui/home/chat/files_preview.dart';
+import '../widgets/dialog.dart';
+import '../widgets/toast.dart';
+import 'extension/extension.dart';
+import 'file.dart';
+
 export 'package:mixin_logger/mixin_logger.dart';
+
+Future<void> showShareLogDialog(
+  BuildContext context, {
+  required ConversationState conversation,
+}) async {
+  final event = await showConfirmMixinDialog(
+    context,
+    'shared log',
+    description: 'share log to ${conversation.name}',
+  );
+  if (event != DialogEvent.positive) {
+    return;
+  }
+  try {
+    showToastLoading();
+    final zipPath = await generateTempFilePath(TempFileType.logZip);
+    await compute(_createZipFile, [mixinLogDirectory.path, zipPath]);
+    final file = File(zipPath);
+    unawaited(showFilesPreviewDialog(context, [file.xFile]));
+  } catch (error, stackTrace) {
+    e('showShareLogDialog', error, stackTrace);
+    showToastFailed(error);
+  } finally {
+    Toast.dismiss();
+  }
+}
+
+Future<void> _createZipFile(List<String> paths) async {
+  i('createZipFile: $paths');
+  final dir = paths[0];
+  final zipPath = paths[1];
+  final files = await Directory(dir).list().whereType<File>().toList();
+  final encoder = ZipFileEncoder()..create(zipPath);
+  for (final file in files) {
+    await encoder.addFile(file);
+  }
+  encoder.close();
+}
