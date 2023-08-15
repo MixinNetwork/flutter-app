5b8be525ae05a23bd1fe2af880453e73e52c6780
Bin <17426470+boyan01@users.noreply.github.com>
2023-08-15 19:56:15 +0800

    support double click title to maximize and unmaximize on macOS (#1335)
    * support double click title to maximize and unmaximize
    
    * only enable for macos

--- lib/widgets/window/move_window.dart
+++ lib/widgets/window/move_window.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
 
+import '../../utils/logger.dart';
 import '../../utils/platform.dart';
 
 class MoveWindowBarrier extends StatelessWidget {
@@ -32,15 +33,42 @@ class MoveWindow extends StatelessWidget {
     super.key,
     this.child,
     this.behavior,
+    this.clickToFullScreen = false,
   });
 
   final Widget? child;
 
   final HitTestBehavior? behavior;
 
+  final bool clickToFullScreen;
+
   @override
   Widget build(BuildContext context) => GestureDetector(
         behavior: behavior,
+        onDoubleTap: clickToFullScreen
+            ? () async {
+                if (!kPlatformIsDarwin) {
+                  // Only enable for macOS.
+                  // Windows and Linux already has a title bar.
+                  return;
+                }
+                if (await windowManager.isMaximized()) {
+                  d('window is maximized, restore');
+                  await windowManager.unmaximize();
+                  if (await windowManager.isMaximized()) {
+                    d('window is still maximized, set to default size');
+                    const windowSize = Size(960, 720);
+                    final position =
+                        await calcWindowPosition(windowSize, Alignment.center);
+                    await windowManager.setBounds(null,
+                        position: position, size: windowSize, animate: true);
+                  }
+                } else {
+                  d('window is not maximized, maximize');
+                  await windowManager.maximize();
+                }
+              }
+            : null,
         onPanStart: (details) {
           if (defaultTargetPlatform == TargetPlatform.windows) {
             // Disable MoveWindow for windows.
@@ -72,7 +100,7 @@ class GlobalMoveWindow extends StatelessWidget {
             alignment: Alignment.topCenter,
             child: SizedBox(
               height: 28,
-              child: MoveWindow(),
+              child: MoveWindow(clickToFullScreen: true),
             ),
           ),
         ],
