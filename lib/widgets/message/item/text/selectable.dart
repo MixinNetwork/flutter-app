5cad09e434662ccb54162a6b862ed8596f40ce43
bin <17426470+boyan01@users.noreply.github.com>
2023-10-11 11:23:51 +0800

    fix dart_code_metrics analysis

--- lib/widgets/message/item/text/selectable.dart
+++ lib/widgets/message/item/text/selectable.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: avoid-unused-parameters
+
 import 'dart:async';
 import 'dart:math';
 
@@ -1140,6 +1142,7 @@ class SelectableRegionState extends State<SelectableRegion>
 
   @override
   void dispose() {
+    widget.focusNode.removeListener(_handleFocusChanged);
     _selectable?.removeListener(_updateSelectionStatus);
     _selectable?.pushHandleLayers(null, null);
     _selectionDelegate.dispose();
