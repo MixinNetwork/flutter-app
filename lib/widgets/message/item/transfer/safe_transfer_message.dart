82c9360b9edb6bf76e7fb3cb2e94c0e3fb84c05d
bin <17426470+boyan01@users.noreply.github.com>
2023-11-02 15:23:59 +0800

    improve

--- lib/widgets/message/item/transfer/safe_transfer_message.dart
+++ lib/widgets/message/item/transfer/safe_transfer_message.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mixin_logger/mixin_logger.dart';
 
+import '../../../../constants/resources.dart';
 import '../../../../db/database_event_bus.dart';
 import '../../../../utils/extension/extension.dart';
 import '../../../../utils/hook.dart';
+import '../../../cache_image.dart';
 import '../../../interactive_decorated_box.dart';
 import '../../message.dart';
 import '../../message_bubble.dart';
-import '../../message_style.dart';
+import '../../message_datetime_and_status.dart';
 import 'transfer_page.dart';
 
 class SafeTransferMessage extends HookConsumerWidget {
@@ -21,10 +24,12 @@ class SafeTransferMessage extends HookConsumerWidget {
 
     var assetIcon = useMessageConverter(converter: (state) => state.assetIcon);
     final snapshotAmount =
-    useMessageConverter(converter: (state) => state.snapshotAmount);
-    final assetSymbol =
-    useMessageConverter(converter: (state) => state.assetSymbol ?? '');
+        useMessageConverter(converter: (state) => state.snapshotAmount);
+    var assetSymbol =
+        useMessageConverter(converter: (state) => state.assetSymbol);
 
+    final snapshotMemo =
+        useMessageConverter(converter: (state) => state.snapshotMemo);
 
     final token = useMemoizedStream(() {
       if (assetId == null || assetIcon != null) {
@@ -42,6 +47,7 @@ class SafeTransferMessage extends HookConsumerWidget {
     }, keys: [assetId, assetIcon]).data;
 
     assetIcon = assetIcon ?? token?.iconUrl;
+    assetSymbol = assetSymbol ?? token?.symbol;
 
     useEffect(() {
       if (assetId != null && assetIcon != null) {
@@ -55,59 +61,95 @@ class SafeTransferMessage extends HookConsumerWidget {
     }, [assetId]);
 
     return MessageBubble(
+      forceIsCurrentUserColor: false,
+      outerTimeAndStatusWidget: const MessageDatetimeAndStatus(),
       child: InteractiveDecoratedBox(
         onTap: () {
           final snapshotId = context.message.snapshotId;
           if (snapshotId == null) return;
           showTransferDialog(context, snapshotId);
         },
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            if (assetIcon != null)
-              SymbolIconWithBorder(
-                symbolUrl: assetIcon,
-                size: 40,
-                chainSize: 12,
-              ),
-            if (assetIcon == null) const SizedBox(width: 40, height: 40),
-            const SizedBox(width: 8),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Flexible(
-                      child: Builder(builder: (context) {
-                        if (snapshotAmount?.isEmpty ?? true) {
-                          return const SizedBox();
-                        }
-
-                        return Text(
-                          snapshotAmount!.numberFormat(),
-                          style: TextStyle(
-                            color: context.theme.text,
-                            fontSize: context.messageStyle.secondaryFontSize,
-                          ),
-                        );
-                      }),
-                    ),
-                  ],
-                ),
-                Text(
-                  assetSymbol,
-                  style: TextStyle(
-                    color: context.theme.secondaryText,
-                    fontSize: context.messageStyle.tertiaryFontSize,
-                  ),
-                ),
-              ],
-            ),
-          ],
+        child: SizedBox(
+          width: 174,
+          child: _SnapshotLayout(
+            assetSymbol: assetSymbol ?? '',
+            assetIcon: assetIcon,
+            snapshotAmount: snapshotAmount,
+            memo: snapshotMemo ?? '',
+          ),
         ),
       ),
     );
   }
+}
+
+class _SnapshotLayout extends StatelessWidget {
+  const _SnapshotLayout({
+    this.assetIcon,
+    this.snapshotAmount,
+    required this.assetSymbol,
+    required this.memo,
+  });
+
+  final String? assetIcon;
+  final String? snapshotAmount;
+  final String assetSymbol;
+  final String memo;
+
+  @override
+  Widget build(BuildContext context) => Stack(
+        children: [
+          Align(
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(Resources.assetsImagesBgSnapshotSvg),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    if (assetIcon == null)
+                      const SizedBox.square(dimension: 16)
+                    else
+                      CacheImage(assetIcon!, width: 16, height: 16),
+                    const SizedBox(width: 4),
+                    Text(
+                      assetSymbol,
+                      style: TextStyle(
+                        color: context.theme.text,
+                        fontSize: 13,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  snapshotAmount ?? '',
+                  style: TextStyle(
+                    color: context.theme.text,
+                    fontFamily: 'MixinCondensed',
+                    fontSize: 36,
+                    fontWeight: FontWeight.w600,
+                    height: 1,
+                  ),
+                ),
+                if (memo.isNotEmpty) ...[
+                  const SizedBox(height: 16),
+                  Text(
+                    memo,
+                    style: TextStyle(
+                      color: context.theme.secondaryText,
+                      fontSize: 12,
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                ] else
+                  const SizedBox(height: 12),
+              ],
+            ),
+          ),
+        ],
+      );
 }
