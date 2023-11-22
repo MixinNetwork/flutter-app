7d7243fbf6cc2ce413a33ad1c35680e559d887fa
bin <17426470+boyan01@users.noreply.github.com>
2023-11-22 21:27:21 +0800

    improve style

--- lib/widgets/message/item/video/video_preview_page.dart
+++ lib/widgets/message/item/video/video_preview_page.dart
@@ -6,12 +6,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:video_player/video_player.dart';
 
 import '../../../../constants/resources.dart';
 import '../../../../utils/extension/extension.dart';
 import '../../../action_button.dart';
+import 'progress_bar.dart';
 
 Future<void> showVideoPreviewPage(BuildContext context, String path) =>
     showDialog(
@@ -24,6 +26,33 @@ final videoPlayerProvider =
     ChangeNotifierProvider<VideoPlayerController>((ref) {
   throw UnimplementedError();
 });
+
+class _CupertinoVideoPlayerStyle extends InheritedWidget {
+  const _CupertinoVideoPlayerStyle({
+    required super.child,
+    required this.background,
+    required this.foreground,
+  });
+
+  final Color background;
+  final Color foreground;
+
+  static _CupertinoVideoPlayerStyle of(BuildContext context) {
+    final result = context
+        .dependOnInheritedWidgetOfExactType<_CupertinoVideoPlayerStyle>();
+    assert(result != null, 'No _CupertinoVideoStyle found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(_CupertinoVideoPlayerStyle old) =>
+      background != old.background || foreground != old.foreground;
+}
+
+extension on BuildContext {
+  _CupertinoVideoPlayerStyle get playerStyle =>
+      _CupertinoVideoPlayerStyle.of(this);
+}
 
 final videoPlayerValueProvider =
     videoPlayerProvider.select((value) => value.value);
@@ -48,13 +77,17 @@ class _VideoPreviewPage extends HookConsumerWidget {
       overrides: [
         videoPlayerProvider.overrideWith((ref) => controller),
       ],
-      child: BackdropFilter(
-        filter: ui.ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-        child: const Stack(
-          children: [
-            VideoFrame(),
-            _Controls(),
-          ],
+      child: _CupertinoVideoPlayerStyle(
+        background: const Color.fromRGBO(41, 41, 41, 0.7),
+        foreground: const Color.fromARGB(255, 200, 200, 200),
+        child: BackdropFilter(
+          filter: ui.ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: const Stack(
+            children: [
+              VideoFrame(),
+              _Controls(),
+            ],
+          ),
         ),
       ),
     );
@@ -86,26 +119,71 @@ class _Controls extends ConsumerWidget {
         onHover: (event) {},
         child: Column(
           children: [
-            const Spacer(),
+            const SizedBox(height: 54),
             Row(
               children: [
-                const Spacer(),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.play_arrow),
-                ),
-                IconButton(
-                  onPressed: () {
-                    ref.read(videoPlayerProvider).pause();
+                _CupertinoButton(
+                  name: Resources.assetsImagesIcCloseSvg,
+                  onTap: () {
+                    ref.read(videoPlayerProvider)
+                      ..pause()
+                      ..dispose();
+                    Navigator.maybePop(context);
                   },
-                  icon: const Icon(Icons.pause),
                 ),
-                const Spacer(),
               ],
             ),
+            const Spacer(),
             const _OperationBar(),
             const SizedBox(height: 36),
           ],
+        ),
+      );
+}
+
+class _CupertinoButton extends StatelessWidget {
+  const _CupertinoButton({
+    required this.onTap,
+    required this.name,
+  });
+
+  final VoidCallback onTap;
+  final String name;
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: onTap,
+        child: _CupertinoContainer(
+          child: SvgPicture.asset(
+            name,
+            width: 24,
+            height: 24,
+            colorFilter: ColorFilter.mode(
+              context.theme.icon,
+              BlendMode.srcIn,
+            ),
+          ),
+        ),
+      );
+}
+
+class _CupertinoContainer extends StatelessWidget {
+  const _CupertinoContainer({required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: BackdropFilter(
+          filter: ui.ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: ColoredBox(
+            color: context.theme.background.withOpacity(0.8),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: child,
+            ),
+          ),
         ),
       );
 }
@@ -115,56 +193,65 @@ class _OperationBar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) => ClipRRect(
-        borderRadius: BorderRadius.circular(8),
+        borderRadius: BorderRadius.circular(10),
         child: Material(
-          color: context.theme.background.withOpacity(0.5),
+          color: context.playerStyle.background,
           child: BackdropFilter(
             filter: ui.ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const SizedBox(height: 16),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ActionButton(
-                      child: const Icon(CupertinoIcons.gobackward_15),
-                      onTap: () {
-                        final position = ref
-                            .read(videoPlayerValueProvider)
-                            .position
-                            .inMilliseconds;
-                        final target = math.max(0, position - 15 * 1000);
-                        ref
-                            .read(videoPlayerProvider)
-                            .seekTo(target.milliseconds);
-                      },
-                    ),
-                    const _PlayPause(),
-                    ActionButton(
-                      child: const Icon(CupertinoIcons.goforward_15),
-                      onTap: () {
-                        final position = ref
-                            .read(videoPlayerValueProvider)
-                            .position
-                            .inMilliseconds;
-                        final target = math.min(
-                            ref
-                                .read(videoPlayerValueProvider)
-                                .duration
-                                .inMilliseconds,
-                            position + 15 * 1000);
-                        ref
-                            .read(videoPlayerProvider)
-                            .seekTo(target.milliseconds);
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 8),
-                const _PlayerProgressBar(),
-                const SizedBox(height: 16),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ActionButton(
+                        child: Icon(
+                          CupertinoIcons.gobackward_15,
+                          color: context.playerStyle.foreground,
+                        ),
+                        onTap: () {
+                          final position = ref
+                              .read(videoPlayerValueProvider)
+                              .position
+                              .inMilliseconds;
+                          final target = math.max(0, position - 15 * 1000);
+                          ref
+                              .read(videoPlayerProvider)
+                              .seekTo(target.milliseconds);
+                        },
+                      ),
+                      const SizedBox(width: 8),
+                      const _PlayPause(),
+                      const SizedBox(width: 8),
+                      ActionButton(
+                        child: Icon(
+                          CupertinoIcons.goforward_15,
+                          color: context.playerStyle.foreground,
+                        ),
+                        onTap: () {
+                          final position = ref
+                              .read(videoPlayerValueProvider)
+                              .position
+                              .inMilliseconds;
+                          final target = math.min(
+                              ref
+                                  .read(videoPlayerValueProvider)
+                                  .duration
+                                  .inMilliseconds,
+                              position + 15 * 1000);
+                          ref
+                              .read(videoPlayerProvider)
+                              .seekTo(target.milliseconds);
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  const _PlayerProgressBar(),
+                ],
+              ),
             ),
           ),
         ),
@@ -183,71 +270,29 @@ class _PlayerProgressBar extends HookConsumerWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        SizedBox(
-          width: 48,
-          child: Center(
-            child: Text(
-              positionText,
-              style: TextStyle(
-                fontSize: 14,
-                color: context.theme.text,
-              ),
-            ),
+        Text(
+          positionText,
+          style: TextStyle(
+            fontSize: 12,
+            color: context.playerStyle.foreground,
           ),
         ),
-        const SizedBox(width: 4),
+        const SizedBox(width: 12),
         ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 400),
-          child: const _PlayerProgressSlider(),
+          constraints: const BoxConstraints(maxWidth: 400, minWidth: 300),
+          child: CupertinoVideoProgressBar(
+            ref.watch(videoPlayerProvider.select((value) => value)),
+          ),
         ),
-        const SizedBox(width: 4),
-        SizedBox(
-          width: 48,
-          child: Center(
-            child: Text(
-              durationText,
-              style: TextStyle(
-                fontSize: 14,
-                color: context.theme.text,
-              ),
-            ),
+        const SizedBox(width: 12),
+        Text(
+          durationText,
+          style: TextStyle(
+            fontSize: 12,
+            color: context.playerStyle.foreground,
           ),
         ),
       ],
-    );
-  }
-}
-
-class _PlayerProgressSlider extends HookConsumerWidget {
-  const _PlayerProgressSlider();
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final userTrackingValue = useState<double?>(null);
-    final position = ref
-        .watch(videoPlayerValueProvider.select((value) => value.position))
-        .inMilliseconds
-        .toDouble();
-    final duration = ref
-        .watch(videoPlayerValueProvider.select((value) => value.duration))
-        .inMilliseconds
-        .toDouble();
-    return Slider(
-      max: duration,
-      value: (userTrackingValue.value ?? position).clamp(
-        0.0,
-        duration,
-      ),
-      onChangeStart: (value) => userTrackingValue.value = value,
-      onChanged: (value) => userTrackingValue.value = value,
-      semanticFormatterCallback: (value) =>
-          value.round().milliseconds.asMinutesSeconds,
-      onChangeEnd: (value) {
-        userTrackingValue.value = null;
-        ref
-            .read(videoPlayerProvider)
-            .seekTo(Duration(milliseconds: value.round()));
-      },
     );
   }
 }
@@ -260,7 +305,7 @@ class _PlayPause extends ConsumerWidget {
     final playing =
         ref.watch(videoPlayerValueProvider.select((value) => value.isPlaying));
     return ActionButton(
-      color: context.theme.icon,
+      color: context.playerStyle.foreground,
       name: playing
           ? Resources.assetsImagesPlayerPauseSvg
           : Resources.assetsImagesPlayerPlaySvg,
