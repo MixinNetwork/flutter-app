4ca71b1cf19af592f8ef773db8a3facd9f1cf3cd
Bin <17426470+boyan01@users.noreply.github.com>
2024-04-24 12:06:43 +0800

    fix type group not working on Windows (#1609)

--- lib/utils/file.dart
+++ lib/utils/file.dart
@@ -40,11 +40,23 @@ Future<bool> saveFileToSystem(
   String? suggestName,
 }) async {
   final targetName = (suggestName ?? file).pathBasename;
+
   d('saveFileToSystem: $file, $targetName');
+
+  final mineType = lookupMimeType(file);
+  final extension = p.extension(targetName);
 
   final path = (await file_selector.getSaveLocation(
     confirmButtonText: context.l10n.save,
     suggestedName: targetName,
+    acceptedTypeGroups: [
+      if (Platform.isWindows)
+        file_selector.XTypeGroup(
+          label: mineType,
+          extensions: [extension],
+          mimeTypes: [if (mineType != null) mineType],
+        ),
+    ],
   ))
       ?.path;
   if (path == null || path.isEmpty) {
